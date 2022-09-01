86d17f0b0bc01b6dc9442b6fb43ceafc67905846
zhengwei.chen <zhengwei.chen@51job.com>
2022-09-01 14:06:29 +0800

    Initial commit

---
+++ ruyuan-eshop-common/src/main/java/com/ruyuan/eshop/common/utils/abc.java
@@ -0,0 +1,14 @@
+package com.ruyuan.eshop.common.utils;
+
+import com.ruyuan.eshop.common.core.AbstractObject;
+import lombok.Data;
+
+@Data
+public class abc extends AbstractObject {
+
+    private String aa;
+
+    private int bb;
+
+    private String cc;
+}
---
+++ ruyuan-eshop-common/src/main/java/com/ruyuan/eshop/common/utils/bbb.java
@@ -0,0 +1,13 @@
+package com.ruyuan.eshop.common.utils;
+
+import lombok.Data;
+
+@Data
+public class bbb {
+
+    private String aa;
+
+    private Integer bb;
+
+    private Integer cc;
+}
